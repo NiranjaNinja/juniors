cbff7f0641fde045e31d5547cbe942b947e360db
Niranjan <code.plagiarism.fyp@gmail.com>
2018-06-09 18:33:21 +0530

    third commit

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 var logincontroller = require('../controllers/logincontroller');
 
-console.log("haiii");
+console.log("helloooo");
 router.post('/',logincontroller.authenticate);
 
 module.exports = router;
